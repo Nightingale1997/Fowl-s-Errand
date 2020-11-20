a0477202c81cea65345b36ddc4e52410d6ded4ee
Kevin Solovjov <kevin.solovjov@gmail.com>
2020-11-20 14:51:07 +0100

    Swinging Rope

---
+++ Assets/Scripts/SwingBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingBall : MonoBehaviour
+{
+
+    public float speed = 2.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        
+        transform.Rotate(Mathf.Sin(Time.time)/2, 0.0f, 0.0f, Space.Self);
+    }
+}
