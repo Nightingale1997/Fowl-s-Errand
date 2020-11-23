0af5d1f4cf9e8b69ff22cd704a67b7a9cac0c754
Kevin Solovjov <kevin.solovjov@gmail.com>
2020-11-23 19:34:46 +0100

    Moving cars on road

---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public float Timer = 2;
+    GameObject carClone;
+    public GameObject car;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0f)
+        {
+            carClone = Instantiate(car, new Vector3(-52.37f, 1.69f, -19.27f), Quaternion.Euler(new Vector3(0, -50, 0))) as GameObject;
+            Timer = 2f;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveCar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour
+{
+    public float carSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+ void Update()
+    {
+        Vector3 carMovement = new Vector3(10, 0, 0) * carSpeed * Time.deltaTime;
+        transform.Translate(carMovement, Space.Self);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("test");
+        if (other.gameObject.tag == "Despawner")
+        {
+            Destroy(gameObject);
+            //or gameObject.SetActive(false);
+        }
+    }
+}
