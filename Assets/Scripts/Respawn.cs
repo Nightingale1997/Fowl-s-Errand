675c1e9f33628bf07f3c1a9d30fcb987e95a1804
Kevin Solovjov <kevin.solovjov@gmail.com>
2020-11-24 17:16:57 +0100

    Removed logging

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -19,10 +19,9 @@ public class Respawn : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
 
-        Debug.Log("Test2");
+
         if (other.gameObject.tag == "Water")
         {
-            Debug.Log("Test3");
             transform.position = new Vector3(-5.31f, 0.5f, -39);
         }
     }
