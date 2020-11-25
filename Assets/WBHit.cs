2436d479969044627b555a5c0634789b9687ec8d
eliovenero <venero@student.chalmers.se>
2020-11-25 08:38:47 +0100

    fixCollision

--- Assets/WBHit.cs
+++ Assets/WBHit.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class WBHit : MonoBehaviour
 {
     public Rigidbody chickenThrown;
+    
     // Start is called before the first frame update
     void Start()
     {
         //chickenThrown = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
@@ -19,12 +21,16 @@ public class WBHit : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-       
-        chickenThrown.AddForce(10f, 10f, 10f);
-        //chickenThrown.constraints = RigidbodyConstraints.None; //let the chicken fall on its sides and face
-        Debug.Log("HIT");
-        
-        //chickenThrown.constraints = RigidbodyConstraints.FreezeRotationX;
-        //chickenThrown.constraints = RigidbodyConstraints.FreezeRotationZ;
+        if (other.gameObject.tag == "Player")
+        {
+
+            chickenThrown.velocity = new Vector3(5f, 5f, 5f);
+            //chickenThrown.AddForce(5f, 5f, 5f);
+            chickenThrown.constraints = RigidbodyConstraints.None; //let the chicken fall on its sides and face
+            Debug.Log("HIT");
+            
+            //chickenThrown.constraints = RigidbodyConstraints.FreezeRotationX;
+            //chickenThrown.constraints = RigidbodyConstraints.FreezeRotationZ;
+        }
     }
 }
